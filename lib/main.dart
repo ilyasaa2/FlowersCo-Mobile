b52df31ca4988a0ffbe174e0b83e8a2200165e49
Ilyasa Abiyyu W <abiyyuilyasa59@gmail.com>
2026-05-15 17:16:50 +0700

    Fix project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
+import 'core/constants.dart';
+import 'ui/auth/login_page.dart';
+import 'ui/auth/register_page.dart';
+import 'ui/auth/forgot_password_page.dart';
+import 'ui/pages/homepage.dart';
+import 'ui/pages/katalog_page.dart';
+import 'ui/pages/keranjang_page.dart';
+import 'ui/pages/profile_page.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        appBar: AppBar(
-          title: Text("Dicee App", style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.deepOrange,
-        ),
-        body: MyApp(),
-      ),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,6 +17,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flowers.co',
+      theme: ThemeData(useMaterial3: true, primarySwatch: Colors.pink),
+
+      initialRoute: '/',
+
+      routes: {
+        '/': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/forgot-password': (context) => const ForgotPasswordPage(),
+        '/home': (context) => const HomePage(),
+        '/katalog': (context) =>
+            const KatalogPage(), // Buat file katalog_page.dart jika belum ada
+        '/keranjang': (context) =>
+            const KeranjangPage(), // Buat file keranjang_page.dart jika belum ada
+        '/profil': (context) => const ProfilPage(),
+      },
+    );
   }
 }
