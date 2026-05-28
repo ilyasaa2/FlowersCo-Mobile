adce80c489ae7666d8b4ed0bbd7aef8de67cdf7d
AzzahraFebia <azafebiascd@gmail.com>
2026-05-28 15:41:14 +0700

    Login

--- lib/ui/auth/login_page.dart
+++ lib/ui/auth/login_page.dart
@@ -17,177 +17,259 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color(0xFFF9EEF2),
       body: SingleChildScrollView(
         child: Column(
           children: [
             // Banner Atas
             Container(
-              height: MediaQuery.of(context).size.height * 0.4,
+              height: 140,
               width: double.infinity,
               decoration: const BoxDecoration(
                 borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(60),
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
                 ),
                 image: DecorationImage(
                   image: AssetImage('assets/images/login_banner.jpg'),
-                  fit: BoxFit.cover,
+                  fit: BoxFit.contain,
                 ),
               ),
             ),
 
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Masuk Akun", style: AppTheme.titleStyle),
-                  const SizedBox(height: 8),
-                  const Text(
-                    "Selamat datang kembali! Silakan masuk ke komunitas Flowers.co.",
-                    style: TextStyle(color: Colors.grey, fontSize: 14),
-                  ),
-                  const SizedBox(height: 35),
-
-                  // Input Email
-                  const Text(
-                    "EMAIL",
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
+            Center(
+              child: Container(
+                width: 340,
+                margin: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 40,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 15,
+                      offset: const Offset(0, 5),
                     ),
-                  ),
-                  TextField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      hintText: "Masukkan email Anda",
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black12),
+                  ],
+                ),
+
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Center(
+                      child: Text(
+                        "Flowers.co",
+                        style: TextStyle(
+                          color: AppColors.primaryPink,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 25),
+                    const SizedBox(height: 10),
+                    const Center(
+                      child: Text("Masuk", style: AppTheme.titleStyle),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      "Selamat datang kembali dalam keanggunan! Silahkan masuk.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.grey, fontSize: 14),
+                    ),
+                    const SizedBox(height: 35),
 
-                  // Input Password
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "PASSWORD",
+                    // Input Email
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "EMAIL",
                         style: TextStyle(
                           fontSize: 10,
                           fontWeight: FontWeight.bold,
                           color: Colors.grey,
                         ),
                       ),
-                      TextButton(
-                        onPressed: () {
-                          // Gunakan pushNamed agar user bisa tekan 'back' kembali ke login
-                          Navigator.pushNamed(context, '/forgot-password');
-                        },
-                        child: const Text(
-                          "Lupa Password?",
-                          style: TextStyle(
-                            fontSize: 10,
+                    ),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(
+                          Icons.email_outlined,
+                          color: Color(0xFFB98B9B),
+                        ),
+
+                        filled: true,
+                        fillColor: const Color(0xFFFFF8FA),
+
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.pink.shade100),
+                        ),
+
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
                             color: AppColors.primaryPink,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  TextField(
-                    controller: passwordController,
-                    obscureText: _isObscure,
-                    decoration: InputDecoration(
-                      hintText: "Masukkan password",
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _isObscure ? Icons.visibility_off : Icons.visibility,
-                        ),
-                        onPressed: () =>
-                            setState(() => _isObscure = !_isObscure),
+
+                        hintText: "Masukkan email Anda",
                       ),
                     ),
-                  ),
+                    const SizedBox(height: 25),
 
-                  const SizedBox(height: 40),
-
-                  // Tombol Masuk
-                  SizedBox(
-                    width: double.infinity,
-                    height: 55,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        String email = emailController.text.trim();
-                        String password = passwordController.text.trim();
-
-                        if (email.isEmpty && password.isEmpty) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text("Email dan password wajib diisi"),
+                    // Input Password
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "PASSWORD",
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
                             ),
-                          );
-                        } else if (email.isEmpty) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text("Email wajib diisi")),
-                          );
-                        } else if (password.isEmpty) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text("Password wajib diisi"),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            // Gunakan pushNamed agar user bisa tekan 'back' kembali ke login
+                            Navigator.pushNamed(context, '/forgot-password');
+                          },
+                          child: const Text(
+                            "Lupa Password?",
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: AppColors.primaryPink,
                             ),
-                          );
-                        } else {
-                          Navigator.pushReplacementNamed(context, '/home');
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.primaryPink,
-                        elevation: 5,
-                        shadowColor: AppColors.primaryPink.withValues(
-                          alpha: 0.3,
+                          ),
                         ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
+                      ],
+                    ),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: _isObscure,
+                      decoration: InputDecoration(
+                        hintText: "Masukkan password",
+                        prefixIcon: const Icon(
+                          Icons.lock_outline,
+                          color: Color(0xFFB98B9B),
                         ),
-                      ),
-                      child: const Text(
-                        "Masuk Sekarang",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+
+                        filled: true,
+                        fillColor: const Color(0xFFFFF8FA),
+
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.pink.shade100),
+                        ),
+
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                            color: AppColors.primaryPink,
+                          ),
+                        ),
+
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isObscure
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                          onPressed: () =>
+                              setState(() => _isObscure = !_isObscure),
                         ),
                       ),
                     ),
-                  ),
 
-                  const SizedBox(height: 20),
+                    const SizedBox(height: 40),
 
-                  // Navigasi ke Daftar Akun
-                  Center(
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/register');
-                      },
-                      child: const Text.rich(
-                        TextSpan(
-                          text: "Belum punya akun? ",
-                          style: TextStyle(color: Colors.grey),
-                          children: [
-                            TextSpan(
-                              text: "Daftar Sekarang",
-                              style: TextStyle(
-                                color: AppColors.primaryPink,
-                                fontWeight: FontWeight.bold,
+                    // Tombol Masuk
+                    SizedBox(
+                      width: double.infinity,
+                      height: 55,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          String email = emailController.text.trim();
+                          String password = passwordController.text.trim();
+
+                          if (email.isEmpty && password.isEmpty) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Email dan password wajib diisi"),
                               ),
-                            ),
-                          ],
+                            );
+                          } else if (email.isEmpty) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Email wajib diisi"),
+                              ),
+                            );
+                          } else if (password.isEmpty) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Password wajib diisi"),
+                              ),
+                            );
+                          } else {
+                            Navigator.pushReplacementNamed(context, '/home');
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.primaryPink,
+                          elevation: 5,
+                          shadowColor: AppColors.primaryPink.withValues(
+                            alpha: 0.3,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: const Text(
+                          "Masuk",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+
+                    const SizedBox(height: 20),
+
+                    // Navigasi ke Daftar Akun
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/register');
+                        },
+                        child: const Text.rich(
+                          TextSpan(
+                            text: "Belum punya akun? ",
+                            style: TextStyle(color: Colors.grey),
+                            children: [
+                              TextSpan(
+                                text: "Daftar Sekarang",
+                                style: TextStyle(
+                                  color: AppColors.primaryPink,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
